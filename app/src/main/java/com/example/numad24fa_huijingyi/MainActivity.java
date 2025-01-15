3b51bcfea34f2f3533726e05dad87def83c1db82
vicky6477 <yi.hu@northeastern.edu>
2025-01-14 17:47:38 -0800

    Add About Me Button for A1

--- app/src/main/java/com/example/numad24fa_huijingyi/MainActivity.java
+++ app/src/main/java/com/example/numad24fa_huijingyi/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.numad24fa_huijingyi;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v ->{
+            Toast.makeText(
+                    MainActivity.this,
+                    "Name: Huijing Yi\nEmail: vicky64777@gmail.com",
+                    Toast.LENGTH_SHORT
+            ).show();
         });
     }
 }
